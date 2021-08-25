36fb9e09cc14caaf4eee770576dfc58f7cfc8981
Bruno Matumoto <brunomatumoto@MacBook-Air-de-Bruno.local>
2021-08-25 19:12:33 -0300

    Add exercise 1 javascript

---
+++ Fundamentos/Bloco4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,17 @@
+const a = 3
+const b = 2
+
+const soma = a + b
+console.log('valor da soma: ', soma)
+
+const subtracao = a - b
+console.log('valor da subtração: ', subtracao)
+
+const multiplicacao = a * b
+console.log('valor da multiplicação: ', multiplicacao)
+
+const divisao = a / b
+console.log('valor da divisão: ', divisao)
+
+const modulo = a % b
+console.log('valor do modulo: ', modulo)
